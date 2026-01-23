1f83d5226800ffc6a3ae1e5c386e2ae52e2ba2a2
naims <naimdrive6@gmail.com>
2026-01-23 11:13:44 +0600

    Home page added.

---
+++ src/components/navbar.tsx
@@ -0,0 +1,33 @@
+import { Briefcase } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <nav className="border-b border-gray-200">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4 ">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 text-xl font-semibold text-primary"
+        >
+          <Briefcase />
+          Job Tracker
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <Link href={"/sign-in"}>
+            <Button
+              variant={"ghost"}
+              className="text-gray-700 hover:text-black"
+            >
+              Log In
+            </Button>
+          </Link>
+          <Link href={"/sign-up"}>
+            <Button className=" hover:bg-primary/90">Sign Up</Button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
